e926bb54c68ccc276a2372153e745439704def12
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-10-14 23:31:24 -0400

    ((nameIP)) v1.3
    Buff / Debuff works
    They don't do anything besides look it but that's okay
    Lights start buggin tf out after a handful of iterations (pc melting bugging)

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -13,6 +13,15 @@ public class GameController : MonoBehaviour
     public GameObject buffAnchor;
     public AnchorController buff;
 
+    public GameObject debuffAnchor;
+    public AnchorController debuff;
+
+    public GameObject dbLight;
+    public GameObject bLight;
+
+    private GameObject db;
+    private GameObject b;
+
     [Header("This is you")]
     public float buffTimer;
     public float debuffTimer;
@@ -21,6 +30,7 @@ public class GameController : MonoBehaviour
     {
         anchors = GameObject.FindGameObjectsWithTag("Anchor");
         Buff();
+        Debuff();
     }
 
     void Update()
@@ -30,26 +40,53 @@ public class GameController : MonoBehaviour
         // select renderer of core
         // change mat to buff / debuff
         // do the thing
-        
 
-        
 
+        GameObject[] bList = GameObject.FindGameObjectsWithTag("BuffLight");
+        if (bList.Length > 1) Destroy(bList[0]);
+
+        GameObject[] dbList = GameObject.FindGameObjectsWithTag("DebuffLight");
+        if (dbList.Length > 1) Destroy(dbList[0]);
 
     }
     public void Buff()
     {
+        if (b != null) Destroy(b);
         Debug.Log("Buffin");
         buffAnchor = anchors[(int)Random.Range(0, anchors.Length)];
         buff = buffAnchor.GetComponent<AnchorController>();
+        if (buff.isDebuffed) Buff();
         buff.isBuffed = true;
-        buff.changeMat(buffMat);        
+        buff.changeMat(buffMat);
+        b = (GameObject)Instantiate(bLight, buffAnchor.transform);
         Invoke("BuffOutro", buffTimer);
+    }
+
+    public void Debuff()
+    {
+        if (db != null) Destroy(db);
+        debuffAnchor = anchors[(int)Random.Range(0, anchors.Length)];
+        debuff = debuffAnchor.GetComponent<AnchorController>();
+        if (debuff.isBuffed) Debuff();
+        debuff.isDebuffed = true;
+        debuff.changeMat(debuffMat);
+        db = (GameObject)Instantiate(dbLight, debuffAnchor.transform);
+        Invoke("debuffOutro", debuffTimer);
+    }
+
+    public void debuffOutro()
+    {
+        debuff.isDebuffed = false;
+        debuff.changeMat(defaultMat);
+        Destroy(db);
+        Debuff();
     }
 
     public void BuffOutro()
     {
         buff.isBuffed = false;
         buff.changeMat(defaultMat);
+        Destroy(b);
         Buff();
     }
 }
